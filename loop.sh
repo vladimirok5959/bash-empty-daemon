5e4a7f82802cbe2a20862019d9260e266e0e9906
Vova Tkach <vladimirok5959@gmail.com>
2018-08-25 23:27:23 +0300

    Refactoring

--- loop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -d "$SETT_DAEMON_PATH/scripts" ]; then
-	for user_script in $SETT_DAEMON_PATH/scripts/*.sh
-	do
-		. $user_script
-	done
-fi
--- run.sh
+++ run.sh
@@ -46,8 +46,13 @@ log_str() {
 }
 
 loop() {
-	# Include body
-	. "$SETT_DAEMON_PATH/loop.sh"
+	# Include user scripts
+	if [ -d "$SETT_DAEMON_PATH/scripts" ]; then
+		for user_script in $SETT_DAEMON_PATH/scripts/*.sh
+		do
+			. $user_script
+		done
+	fi
 }
 
 start() {
